33ab183967e4c2a60782ff6e5521a9805cd7b633
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-07-20 00:02:25 +0530

    deleted unwanted consoles in admin controller

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -1,7 +1,7 @@
-const user = require('../../models/userModel');
-const orderModel = require('../../models/orderModel');
-const productModel = require('../../models/productModel');
-const categoryModel = require('../../models/categoryModel')
+const user = require("../../models/userModel");
+const orderModel = require("../../models/orderModel");
+const productModel = require("../../models/productModel");
+const categoryModel = require("../../models/categoryModel");
 const bcryptjs = require("bcryptjs");
 
 // Code for load the login page.
@@ -32,14 +32,10 @@ const loginPost = async (req, res) => {
     const passwordMatch = await bcryptjs.compare(password, admin.password);
 
     if (passwordMatch) {
-    
       req.session.admin_id = admin._id;
 
-      console.log('session done. successfull')
-
-      return res.redirect("/admin/home"); 
+      return res.redirect("/admin/home");
     } else {
-      
       return res.render("admin/login", {
         message: "Invalid email or password.",
       });
@@ -53,10 +49,9 @@ const loginPost = async (req, res) => {
 // Code for load the home page.
 const loadHome = async (req, res) => {
   try {
-
-    const orders = await orderModel.find({orderStatus:'delivered'})
-    const products = await productModel.find({})
-    const categories = await categoryModel.find({})
+    const orders = await orderModel.find({ orderStatus: "delivered" });
+    const products = await productModel.find({});
+    const categories = await categoryModel.find({});
 
     let labels;
 
@@ -65,7 +60,7 @@ const loadHome = async (req, res) => {
     const filter = req.query.filter;
 
     const revenue = await orderModel.aggregate([
-      { $match: { orderStatus: 'delivered' } }, 
+      { $match: { orderStatus: "delivered" } },
       {
         $group: {
           _id: null,
@@ -74,156 +69,188 @@ const loadHome = async (req, res) => {
       },
     ]);
 
-    if (filter === 'yearly') {
-
+    if (filter === "yearly") {
       const currentYear = new Date().getFullYear();
 
-const years = [currentYear - 4, currentYear - 3, currentYear - 2, currentYear - 1, currentYear];
-labels = years
+      const years = [
+        currentYear - 4,
+        currentYear - 3,
+        currentYear - 2,
+        currentYear - 1,
+        currentYear,
+      ];
+      labels = years;
 
-const yearlyRevenues = [];
+      const yearlyRevenues = [];
 
-for (const year of years) {
-    const startOfYear = new Date(year, 0, 1);
-    const endOfYear = new Date(year + 1, 0, 1);
-    endOfYear.setMilliseconds(endOfYear.getMilliseconds() - 1);
+      for (const year of years) {
+        const startOfYear = new Date(year, 0, 1);
+        const endOfYear = new Date(year + 1, 0, 1);
+        endOfYear.setMilliseconds(endOfYear.getMilliseconds() - 1);
 
-    const yearlyRevenue = await orderModel.aggregate([
-        {
+        const yearlyRevenue = await orderModel.aggregate([
+          {
             $match: {
-                orderStatus: 'delivered',
-                orderDate: {
-                    $gte: startOfYear,
-                    $lt: endOfYear
-                }
-            }
+              orderStatus: "delivered",
+              orderDate: {
+                $gte: startOfYear,
+                $lt: endOfYear,
+              },
+            },
+          },
+          {
+            $group: {
+              _id: { $year: "$orderDate" },
+              yearlyRevenue: { $sum: "$totalPrice" },
+            },
+          },
+        ]);
+
+        yearlyRevenues.push({ year, yearlyRevenue });
+      }
+
+      graphValue = Array(5).fill(0);
+
+      yearlyRevenues.forEach((yearData, index) => {
+        graphValue[index] =
+          yearData.yearlyRevenue.length > 0
+            ? yearData.yearlyRevenue[0].yearlyRevenue
+            : 0;
+      });
+    } else {
+      labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+      const currentMonth = new Date();
+
+      const startOfMonth = new Date(currentMonth.getFullYear(), 0, 1);
+      const endOfMonth = new Date(currentMonth.getFullYear() + 1, 0, 1);
+      endOfMonth.setMilliseconds(endOfMonth.getMilliseconds() - 1);
+
+      const monthlyRevenue = await orderModel.aggregate([
+        {
+          $match: {
+            orderStatus: "delivered",
+            orderDate: {
+              $gte: startOfMonth,
+              $lt: endOfMonth,
+            },
+          },
         },
         {
-            $group: {
-                _id: { $year: '$orderDate' },
-                yearlyRevenue: { $sum: '$totalPrice' }
-            }
-        }
-    ]);
-
-    yearlyRevenues.push({ year, yearlyRevenue });
-}
-
-   graphValue = Array(5).fill(0);
-
-yearlyRevenues.forEach((yearData, index) => {
-    graphValue[index] = yearData.yearlyRevenue.length > 0 ? yearData.yearlyRevenue[0].yearlyRevenue : 0;
-});
-
-
-    } else {
-
-    labels = [1, 2, 3, 4, 5,6, 7, 8,9, 10, 11, 12];
-
-
-    const currentMonth = new Date();
-
-    const startOfMonth = new Date(currentMonth.getFullYear(),0,1);
-    const endOfMonth = new Date(currentMonth.getFullYear()+1,0,1);
-    endOfMonth.setMilliseconds(endOfMonth.getMilliseconds()-1);
-
-    const monthlyRevenue = await orderModel.aggregate([
-      {
-        $match : {
-          orderStatus : 'delivered',
-          orderDate : {
-            $gte:startOfMonth,
-            $lt: endOfMonth
-          }
-        }
-      },
-      {
-        $group : {
-          _id : { $month : '$orderDate'},
-          monthlyRevenue : { $sum : '$totalPrice'}
-        }
-      }
-    ]);
+          $group: {
+            _id: { $month: "$orderDate" },
+            monthlyRevenue: { $sum: "$totalPrice" },
+          },
+        },
+      ]);
 
       graphValue = Array(12).fill(0);
 
-    monthlyRevenue .forEach(entry => {
-      const monthIndex = entry._id -1;
-      graphValue[monthIndex] = entry.monthlyRevenue;
-    })
+      monthlyRevenue.forEach((entry) => {
+        const monthIndex = entry._id - 1;
+        graphValue[monthIndex] = entry.monthlyRevenue;
+      });
+    }
 
-  }
+    const COD = await orderModel.countDocuments({
+      payment: "cash on delivery",
+      orderStatus: "delivered",
+    });
 
-    const COD = await orderModel.countDocuments({ payment : 'cash on delivery',orderStatus:'delivered'});
+    const Wallet = await orderModel.countDocuments({
+      payment: "wallet",
+      orderStatus: "delivered",
+    });
 
-    const Wallet = await orderModel.countDocuments({ payment : 'wallet',orderStatus:'delivered'});
-
-    const Razorpay = await orderModel.countDocuments({ payment : 'Razorpay',orderStatus:'delivered'});
+    const Razorpay = await orderModel.countDocuments({
+      payment: "Razorpay",
+      orderStatus: "delivered",
+    });
 
     const topSellingProducts = await orderModel.aggregate([
-      { $match: { orderStatus: 'delivered' } },
+      { $match: { orderStatus: "delivered" } },
       { $unwind: "$product" },
       {
         $lookup: {
           from: "products",
           localField: "product.productId",
           foreignField: "_id",
-          as: "joinedProduct"
-        }
+          as: "joinedProduct",
+        },
       },
-      { $unwind: "$joinedProduct" }, 
+      { $unwind: "$joinedProduct" },
       {
         $group: {
-          _id: "$joinedProduct._id", 
-          productName: { $first: "$joinedProduct.name" }, 
-          totalSold: { $sum: "$product.quantity" }, 
-        }
+          _id: "$joinedProduct._id",
+          productName: { $first: "$joinedProduct.name" },
+          totalSold: { $sum: "$product.quantity" },
+        },
       },
       { $sort: { totalSold: -1 } },
       { $limit: 3 },
     ]);
-    
-    const topProductLabels = topSellingProducts.map(product => product.productName);
-    const topProductCounts = topSellingProducts.map(product => product.totalSold);
+
+    const topProductLabels = topSellingProducts.map(
+      (product) => product.productName
+    );
+    const topProductCounts = topSellingProducts.map(
+      (product) => product.totalSold
+    );
 
     const topSellingCategories = await orderModel.aggregate([
-      { $match: { orderStatus: 'delivered' } },
-      { $unwind: "$product" }, 
+      { $match: { orderStatus: "delivered" } },
+      { $unwind: "$product" },
       {
-        $lookup: { 
+        $lookup: {
           from: "products",
           localField: "product.productId",
           foreignField: "_id",
-          as: "joinedProduct"
-        }
+          as: "joinedProduct",
+        },
       },
-      { $unwind: "$joinedProduct" }, 
+      { $unwind: "$joinedProduct" },
       {
-        $lookup: { 
+        $lookup: {
           from: "categories",
-          localField: "joinedProduct.category", 
+          localField: "joinedProduct.category",
           foreignField: "_id",
-          as: "joinedCategory"
-        }
+          as: "joinedCategory",
+        },
       },
-      { $unwind: "$joinedCategory" }, 
+      { $unwind: "$joinedCategory" },
       {
         $group: {
-          _id: "$joinedCategory._id", 
-          categoryName: { $first: "$joinedCategory.name" }, 
-          totalSold: { $sum: "$product.quantity" }, 
-        }
+          _id: "$joinedCategory._id",
+          categoryName: { $first: "$joinedCategory.name" },
+          totalSold: { $sum: "$product.quantity" },
+        },
       },
-      { $sort: { totalSold: -1 } }, 
-      { $limit: 3 }, 
+      { $sort: { totalSold: -1 } },
+      { $limit: 3 },
     ]);
-    
-    const topCategoryLabels = topSellingCategories.map(category => category.categoryName);
-    const topCategoryCounts = topSellingCategories.map(category => category.totalSold);
 
+    const topCategoryLabels = topSellingCategories.map(
+      (category) => category.categoryName
+    );
+    const topCategoryCounts = topSellingCategories.map(
+      (category) => category.totalSold
+    );
 
-
-    res.render("admin/home", {orders,products,categories,revenue,graphValue,labels,COD,Wallet,Razorpay,topProductLabels,topProductCounts,topCategoryLabels,topCategoryCounts});
+    res.render("admin/home", {
+      orders,
+      products,
+      categories,
+      revenue,
+      graphValue,
+      labels,
+      COD,
+      Wallet,
+      Razorpay,
+      topProductLabels,
+      topProductCounts,
+      topCategoryLabels,
+      topCategoryCounts,
+    });
   } catch (error) {
     console.log(error.message);
   }
@@ -233,5 +260,5 @@ yearlyRevenues.forEach((yearData, index) => {
 module.exports = {
   loadHome,
   loginLoad,
-  loginPost
+  loginPost,
 };
